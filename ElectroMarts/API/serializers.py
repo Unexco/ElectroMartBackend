fb8cf69308930f61c1fb3804e6b1a6007eb91385
eunicefegorumukoro <umukorofegor12@gmail.com>
2024-10-06 11:45:45 -0700

    backend complete

---
+++ ElectroMarts/API/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from ..models import Category, Product
+#from ..models import Product
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        read_only_fields = ['id','name']
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    class Meta:
+        model = Product
+        fields = '__all__'
