11855849d1a0426bbbff88539d5a1cc026bae332
eunicefegorumukoro <umukorofegor12@gmail.com>
2024-10-06 13:09:51 -0700

    working properly

--- ElectroMart/urls.py
+++ ElectroMart/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ElectroMarts.views import secret_key_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ElectroMarts.API.urls')),
+    path('secret-key/', secret_key_view, name='secret-key'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ElectroMarts/views.py
+++ ElectroMarts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+
+def secret_key_view(request):
+    return HttpResponse(f'SECRET_KEY is: {settings.SECRET_KEY}')
 
 # Create your views here.
