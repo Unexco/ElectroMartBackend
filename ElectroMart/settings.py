fa2679053c8b4701f8c121622862bfb9089613b4
eunicefegorumukoro <umukorofegor12@gmail.com>
2024-10-09 22:37:54 +0100

    deploying render database

--- ElectroMart/settings.py
+++ ElectroMart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,17 +94,21 @@ WSGI_APPLICATION = 'ElectroMart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'electromartsdb',
-        'USER': 'postgres',
-        'PASSWORD': 'vnp-1234',
-        'HOST': 'localhost',
-        #'HOST': '35.160.120.126',
-        'PORT':5432,
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'electromartsdb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vnp-1234',
+#         'HOST': 'localhost',
+#         #'HOST': '35.160.120.126',
+#         'PORT':5432,
 
-    }
+#     }
+# }
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
